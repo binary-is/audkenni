51a44e6dafcf8dda39f0efc930299605a8c4d76c
Helgi Hrafn Gunnarsson <helgi@binary.is>
2024-09-13 13:47:47 +0000

    Example-use script updated to take arguments instead of hard-coding example data.

--- example_use.py
+++ example_use.py
@@ -5,10 +5,35 @@
 #     from dotenv import load_dotenv
 #     load_dotenv()
 
+import sys
 from audkenni import see_some_id  # Needs `load_dotenv()` before running.
 
-person = see_some_id("5551234", "Innskráning")
 
-print("Name: %s" % person["name"])
-print("SSN: %s" % person["nationalRegisterId"])
-print("Signature: %s" % person["signature"])
+def usage():
+    print('Usage: ./example_use.py <phone-number> "<message>"')
+    print()
+    quit()
+
+
+def parse_arguments(argv):
+    try:
+        phone_number = argv[1]
+        message = argv[2]
+    except IndexError:
+        usage()
+
+    return phone_number, message
+
+
+def main(argv):
+    phone_number, message = parse_arguments(argv)
+
+    person = see_some_id(phone_number, message)
+
+    print("Name: %s" % person["name"])
+    print("SSN: %s" % person["nationalRegisterId"])
+    print("Signature: %s" % person["signature"])
+
+
+if __name__ == "__main__":
+    main(sys.argv)
